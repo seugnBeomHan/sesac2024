b7d96d7e2f7def72d2f0cc47c513565164a6d27f
seungBeomHan <essence.of.study@gmail.com>
2024-06-05 12:17:36 +0900

    [main / REFECT] 문제 2 범위 수정, 문제 4 매개변수 숫자 자릿수 변환 함수 작성 후 변경

--- src/06.04_homework.js
+++ src/06.04_homework.js
@@ -22,7 +22,7 @@ const problem_1 = () => {
     }
 
     // 매번 비교를 하지 않는 방법
-    console.log(`\n[1번 문제, 매번 비교하지 않는 방법]\n`);
+    console.log(`\n[1번 문제, 매번 비교하지 않는 방법]`);
 
     let i = 0.1;
     const endVer2 = 0.9;
@@ -36,16 +36,16 @@ const problem_1 = () => {
  * 2. 1 ~ 10 사이의 정수에 대해 제곱근을 소숫점 3자리까지 출력하시오.
  */
 const problem_2 = () => {
-    console.log(`\n[2번 문제]\n`);
+    console.log(`\n[2번 문제]`);
 
     const end = 10;
-    for (let i = 0; i < end; i += 1) {
+    for (let i = 0; i <= end; i += 1) {
         console.log(Math.sqrt(i) % 1 !== 0 ? `${i}의 제곱근 : ` + Math.sqrt(i).toFixed(3) : `${i}의 제곱근 : ` + Math.sqrt(i));
     }
 
     // 문자열 및 제곱근 중복 제거 버전
-    console.log(`\n[2번 문제 중복 제거 버전]\n`);
-    for (let i = 0; i < end; i += 1) {
+    console.log(`\n[2번 문제 중복 제거 버전]`);
+    for (let i = 0; i <= end; i += 1) {
         const nowSqrt = Math.sqrt(i);
         const str = `${i}의 제곱근 : `;
 
@@ -59,7 +59,7 @@ const problem_2 = () => {
  *  3-2. 더 간단한 방법 고민 
  */
 const problem_3 = () => {
-    console.log(`\n[3번 문제]\n`);
+    console.log(`\n[3번 문제]`);
 
     const date = new Date();
     const today = date.getDay();
@@ -94,17 +94,17 @@ const problem_3 = () => {
     console.log(`오늘은 ${todayStr}요일 입니다.`);
 
     // 다른 버전 1
-    console.log(`\n[3번 문제 다른 버전]\n`);
+    console.log(`\n[3번 문제 다른 버전]`);
     const dayArr = [`일`, `월`, `화`, `수`, `목`, `금`, `토`];
     console.log(`오늘은 ${dayArr[today]}요일 입니다.`);
 
     // 다른 버전 2
-    console.log(`\n[3번 문제 다른 버전 2]\n`);
+    console.log(`\n[3번 문제 다른 버전 2]`);
     const dayStr = `일월화수목금토`;
     console.log(`오늘은 ${dayStr[today]}요일 입니다.`);
 
     // 다른 버전 3
-    console.log(`\n[3번 문제 다른 버전 3]\n`);
+    console.log(`\n[3번 문제 다른 버전 3]`);
     const daySpaceStr = `일 월 화 수 목 금 토`;
     const daySplit = daySpaceStr.split(' ');
     console.log(`오늘은 ${daySplit[today]}요일 입니다.`);
@@ -120,11 +120,16 @@ const problem_3 = () => {
  * 0.34 + 0.226 = 0.56600000...1
  */
 const problem_4 = () => {
-    console.log(`\n[4번 문제]\n`);
+    console.log(`\n[4번 문제]`);
+
+    const getDigit = (number) => {
+        return (number + '').length;
+    }
+
     const addPoints = (num1, num2) => {
-        const CORRECT_VALUE = 2; // string으로 변환하는 과정에서 0.에 대한 length 제거 용도
-        const num1Len = ((num1 + '').length) - CORRECT_VALUE;
-        const num2Len = ((num2 + '').length) - CORRECT_VALUE;
+        const CORRECT_VALUE = 2; // string으로 변환하는 과정에서 자릿수를 맞추기 위한 길이 제거 용도
+        const num1Len = getDigit(num1) - CORRECT_VALUE;
+        const num2Len = getDigit(num2) - CORRECT_VALUE;
         const fixedDigit = num1Len >= num2Len ? num1Len : num2Len;
 
         return (num1 + num2).toFixed(fixedDigit);
