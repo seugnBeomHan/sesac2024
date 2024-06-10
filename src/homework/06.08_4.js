7468a34dd0332cc7af7730c750ad12b11a73e55b
seungBeomHan <essence.of.study@gmail.com>
2024-06-10 16:37:28 +0900

    [main / CREATE] 구조분해할당 과제 4, First Commit

---
+++ src/homework/06.08_4.js
@@ -0,0 +1,11 @@
+const user = { name: 'Hong', passwd: 'xyz', addr: 'Seoul' };
+
+const getValueExceptInitial = (inputKey) => {
+    const { [inputKey]: value } = user;
+    const [, ...result] = value;
+    return result.join('');
+}
+
+console.log(getValueExceptInitial('name')); // 'ong'
+console.log(getValueExceptInitial('passwd')); // 'yz'
+console.log(getValueExceptInitial('addr')); // 'eoul'
