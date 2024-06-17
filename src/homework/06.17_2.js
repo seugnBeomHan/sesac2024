c86e6fa32b76701c0be9dcf8262af091d367feac
seungBeomHan <essence.of.study@gmail.com>
2024-06-17 15:26:05 +0900

    [main / FINISH] 과제 2번 완료, 피보나치 수열 3가지 방법 구현

--- src/homework/06.17_2.js
+++ src/homework/06.17_2.js
@@ -1,6 +1,6 @@
 const isZeroOrLess = input => {
     return input <= 0;
-}
+};
 
 // 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
 
@@ -31,29 +31,74 @@ const fibonacciRecursive = n => {
         }
         return recursive(nRecur - 1) + recursive(nRecur - 2);
     })(n);
+};
+
+// memoized
+const fibonacciMemoized = n => {
+    if (isZeroOrLess(n)) return 0;
+
+    const storage = {};
+
+    return (function recursive(nRecur) {
+        if (nRecur <= 2) {
+            return storage[nRecur] = 1;
+        }
+        const curFiboVal = storage[nRecur];
+
+        return curFiboVal === undefined ?
+            storage[nRecur] = recursive(nRecur - 1) + recursive(nRecur - 2) :
+            curFiboVal;
+    })(n);
+};
+
+// time check
+const timeCheck = (n, ...fns) => {
+    for (const fn of fns) {
+        console.time(`${fn.name} time`);
+        fn(n);
+        console.timeEnd(`${fn.name} time`);
+    }
 }
 
+
 // test
-const resultPrint = (testFn, n) => {
-    console.log(`\nTest Function : ${testFn.name}\n`);
+const resultPrint = (testFns, n) => {
+    console.log(`\nTest Function : ${testFns.name}\n`);
 
     if (n <= 0) {
-        console.log(testFn(n));
+        console.log(testFns(n));
         return;
     }
 
     for (let i = 1; i <= n; i += 1) {
-        console.log(`i: ${i} / fibo : ${testFn(i)}`);
+        console.log(`i: ${i} / fibo : ${testFns(i)}`);
     }
 }
 
-const resultEqual = (testFn1, testFn2, n) => {
-    console.log(`\nTest Function : ${testFn1.name}, ${testFn2.name}\n`);
-
+const resultEqual = (n, ...testFns) => {
     if (isZeroOrLess(n)) return 0;
 
+    const testFnsCount = testFns.length;
+
     for (let i = 1; i <= n; i += 1) {
-        console.log(`${i}: ${testFn1(i) === testFn2(i)}`);
+        let prev = -1;
+        let valid = true;
+        for (let j = 0; j < testFnsCount; j += 1) {
+            const curVal = testFns[j](i);
+            if (prev === -1) {
+                prev = curVal;
+                continue;
+            }
+
+            if (prev !== -1) {
+                prev === curVal ? prev = curVal : valid = false;
+            }
+        }
+        if (!valid) {
+            console.log(`${i} 번째 값 불일치`);
+            return;
+        }
+        console.log(valid);
     }
 }
 resultPrint(fibonacciLoop, -1);
@@ -62,4 +107,8 @@ resultPrint(fibonacciLoop, 30);
 resultPrint(fibonacciRecursive, -1);
 resultPrint(fibonacciRecursive, 0);
 resultPrint(fibonacciRecursive, 30);
-resultEqual(fibonacciLoop, fibonacciRecursive, 30);
+resultPrint(fibonacciMemoized, -1);
+resultPrint(fibonacciMemoized, 0);
+resultPrint(fibonacciMemoized, 30);
+resultEqual(30, fibonacciLoop, fibonacciRecursive, fibonacciMemoized);
+timeCheck(40, fibonacciLoop, fibonacciRecursive, fibonacciMemoized);
