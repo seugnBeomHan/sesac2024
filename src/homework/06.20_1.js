3ed978d3ecaf3cc142c4f2bf9997d06bea9be247
seungBeomHan <essence.of.study@gmail.com>
2024-06-22 14:59:39 +0900

    [main / REFECT] 리팩토링 진행, 변수 간소화

--- src/homework/06.20_1.js
+++ src/homework/06.20_1.js
@@ -17,32 +17,25 @@ const once = (fn, repeatSec = 1) => {
 
 // 반복 실행
 const onceRepeat = (fn, repeatSec = 1) => {
-    const DEFAULT_INTERVAL_REPEAT = 100;
-    const maxIntervalCount = (repeatSec * 1000) / DEFAULT_INTERVAL_REPEAT;
-    let curIntervalCount = 0;
+    const DEFAULT_INTERVAL_REPEAT = 1000;
     const excuteQueue = [];
     let isNotExcuted = true;
 
     const cacheIsNotEmpty = () => excuteQueue.length !== 0;
     const cacheIsEmpty = () => excuteQueue.length === 0;
-    const intervalCountIsSame = () => curIntervalCount === maxIntervalCount;
-    const increaseIntervalCount = () => curIntervalCount += 1;
 
     const timer = setInterval(() => {
-        if (cacheIsEmpty()) {
-            curIntervalCount = 0;
-            clearInterval(timer);
-            return;
-        }
-
-        increaseIntervalCount();
-        if (cacheIsNotEmpty() && intervalCountIsSame()) {
-            curIntervalCount = 0;
+        if (cacheIsNotEmpty()) {
             console.log(fn(...excuteQueue[0]));
             excuteQueue.shift();
             return;
         }
-    }, DEFAULT_INTERVAL_REPEAT);
+        if (cacheIsEmpty()) {
+            isNotExcuted = true;
+            clearInterval(timer);
+            return;
+        }
+    }, repeatSec * DEFAULT_INTERVAL_REPEAT);
 
     return (num1, num2) => {
         if (isNotExcuted) {
@@ -54,13 +47,13 @@ const onceRepeat = (fn, repeatSec = 1) => {
     };
 }
 
-// 한 번 실행 후 나머지 실행 x
-const onceExcute = once((x, y) => `금일 운행금지 차량은 끝번호 ${x}, ${y}입니다!`);
-console.log(onceExcute(1, 2));
-console.log(onceExcute(3, 4));
-console.log(onceExcute(5, 6));
-console.log(onceExcute(7, 8));
-console.log(onceExcute(9, 0));
+// // 한 번 실행 후 나머지 실행 x
+// const onceExcute = once((x, y) => `금일 운행금지 차량은 끝번호 ${x}, ${y}입니다!`);
+// console.log(onceExcute(1, 2));
+// console.log(onceExcute(3, 4));
+// console.log(onceExcute(5, 6));
+// console.log(onceExcute(7, 8));
+// console.log(onceExcute(9, 0));
 
 // 한 번 실행 후 나머지 실행 큐잉 > 기본 1초마다 반복, 설정 가능
 const onceExcutes = onceRepeat((x, y) => `금일 운행금지 차량은 끝번호 ${x}, ${y}입니다!`);
