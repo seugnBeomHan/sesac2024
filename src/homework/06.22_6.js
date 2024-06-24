055996611856750a02d022b5a08ca6d49e2f35e9
seungBeomHan <essence.of.study@gmail.com>
2024-06-24 16:30:44 +0900

    [main / REFECT] 과제6, 테스트 케이스 수정 및 reduce 내부 loop > forEach로 수정

--- src/homework/06.22_6.js
+++ src/homework/06.22_6.js
@@ -8,13 +8,13 @@ const arr = [1, 2, 3, 4, 5];
 
 const go = (array, funcs) => {
     return array.reduce((acc, n) => {
-        for (const func of funcs) {
+        funcs.forEach(func => {
             n = func(n);
-        }
+        });
         return [...acc, n];
     }, []);
 };
 
-assert.deepStrictEqual(go(arr, [square, cube, sqrt]), arr.map(a => a ** 2).map(a => Math.sqrt(a)).map(a => a ** 3));
+assert.deepStrictEqual(go(arr, [square, cube, sqrt]), arr.map(a => a ** 2).map(a => a ** 3).map(a => Math.sqrt(a)));
 assert.deepStrictEqual(go(arr, [sqrt, square, cube]), arr.map(a => Math.sqrt(a)).map(a => a ** 2).map(a => a ** 3));
 assert.deepStrictEqual(go(arr, [cube, sqrt, square]), arr.map(a => a ** 3).map(a => Math.sqrt(a)).map(a => a ** 2));
