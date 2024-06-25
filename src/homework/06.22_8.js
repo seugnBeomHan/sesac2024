44ef93a004fe097dd95b87dbfd7b26ae2d3887e6
seungBeomHan <essence.of.study@gmail.com>
2024-06-25 11:55:12 +0900

    [main / REFECT] 과제8 리펙터링 완료, keypair O(n) 속도

--- src/homework/06.22_8.js
+++ src/homework/06.22_8.js
@@ -1,22 +1,40 @@
 import assert from 'node:assert/strict';
 
-const keyPair = (array, pair) => {
-    const ret = {};
-    const len = array.length;
+const keyPairMap = (array, pair) => {
+    const valueMap = new Map();
 
-    const isPair = (target, num) => target + num === pair;
+    for (let i = 0; i < array.length; i += 1) {
+        const cur = array[i];
+        const matchIdx = valueMap.get(pair - cur);
 
-    for (let i = 0; i < len; i += 1) {
-        const target = array[i];
-        for (let j = i + 1; j < len; j += 1) {
-            if (isPair(target, array[j])) return [i, j];
-        }
+        if (matchIdx !== undefined) return [matchIdx, i];
+        if (!(valueMap.has(cur))) valueMap.set(cur, i);
     }
     return;
 };
+
+// n^2
+// const keyPair = (array, pair) => {
+//     const len = array.length;
+//     const isPair = (target, num) => target + num === pair;
+
+//     for (let i = 0; i < len; i += 1) {
+//         const target = array[i];
+//         for (let j = i + 1; j < len; j += 1) {
+//             if (isPair(target, array[j])) return [i, j];
+//         }
+//     }
+//     return;
+// };
 
 assert.deepStrictEqual(keyPair([1, 3, 4, 5], 7), [1, 2]);
 assert.deepStrictEqual(keyPair([1, 4, 45, 6, 10, 8], 16), [3, 4]);
 assert.deepStrictEqual(keyPair([1, 2, 4, 3, 6], 10), [2, 4]);
 assert.deepStrictEqual(keyPair([1, 2, 3, 4, 5, 7], 9), [1, 5]);
 assert.deepStrictEqual(keyPair([1, 2, 3, 4, 5, 7], 12), [4, 5]);
+
+assert.deepStrictEqual(keyPairMap([1, 3, 4, 5], 7), [1, 2]);
+assert.deepStrictEqual(keyPairMap([1, 4, 45, 6, 10, 8], 16), [3, 4]);
+assert.deepStrictEqual(keyPairMap([1, 2, 4, 3, 6], 10), [2, 4]);
+assert.deepStrictEqual(keyPairMap([1, 2, 3, 4, 5, 7], 9), [3, 4]);
+assert.deepStrictEqual(keyPairMap([1, 2, 3, 4, 5, 7], 12), [4, 5]);
