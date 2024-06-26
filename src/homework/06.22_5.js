7eacfe80d0d169479c3801e441b678e74b546764
seungBeomHan <essence.of.study@gmail.com>
2024-06-26 11:35:46 +0900

    [main / REFECT] 과제5, reduce default arg 없을 때 전달 된 Array의 첫 번째 값으로 초기화 할 수 있도록 변경

--- src/homework/06.22_5.js
+++ src/homework/06.22_5.js
@@ -3,16 +3,21 @@
 
 import assert from 'node:assert/strict';
 
-const reduce = (array, fn, acc = 0, thisArg) => {
-    array.forEach((e) => {
-        acc = fn(acc, e, thisArg);
-    });
+const reduce = (array, fn, acc, thisArg) => {
+
+    let i = 0;
+    acc = acc ?? array[i++];
+
+    for (; i < array.length; i += 1) {
+        acc = fn(acc, array[i], i, thisArg);
+    }
     return acc;
 }
 
 assert.deepStrictEqual(reduce([1, 2, 3], (acc, cur) => acc + cur, 0), 6);
 assert.deepStrictEqual(reduce([1, 2, 3, 4, 5], (acc, cur) => acc + cur), 15);
-assert.deepStrictEqual(reduce([1, 2, 3, 4, 5], (acc, cur) => acc * cur, 1), 120);
-assert.deepStrictEqual(reduce([2, 2, 2], (acc, cur) => acc * cur, 1), 8);
-assert.deepStrictEqual(reduce([3, 3, 3], (acc, cur) => acc * cur, 0), 0);
-assert.deepStrictEqual(reduce([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], (acc, cur) => acc + cur, 0), 55);
+assert.deepStrictEqual(reduce([1, 2, 3, 4, 5], (acc, cur) => acc * cur), 120);
+assert.deepStrictEqual(reduce([2, 2, 2], (acc, cur) => acc * cur), 8);
+assert.deepStrictEqual(reduce([3, 3, 3], (acc, cur) => acc * cur), 27);
+assert.deepStrictEqual(reduce([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], (acc, cur) => acc + cur), 55);
+assert.deepStrictEqual(reduce(['han', 'seung', 'beom'], (acc, cur) => acc += cur), 'hanseungbeom');
