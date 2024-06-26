c05163b70d59009d8761e543aea8bbc8f8639c68
seungBeomHan <essence.of.study@gmail.com>
2024-06-26 10:15:08 +0900

    [main / REFECT] 과제1 리펙터링 진행. pop, shift 함수,  loop > slice로 변경

--- src/homework/06.22_1.js
+++ src/homework/06.22_1.js
@@ -11,14 +11,25 @@ const push = (array, ...values) => {
 const pop = (array, count = 1) => {
     const arrLen = array.length;
 
-    if (count < 1 || count > arrLen) return;
-    if (count === 1) return array[arrLen - 1];
-    return array.filter((_, i) => i >= arrLen - count);
+    if (count === 0 ||
+        count > arrLen ||
+        count <= -arrLen) return;
+
+    count = count < 0 ? count + arrLen : arrLen - count;
+    const ret = array.slice(count, arrLen);
+
+    return ret.length === 1 ? ret[0] : ret;
 };
 
 const shift = (array, count = 1) => {
-    if (count < 1 || count > array.length) return;
-    return array.filter((_, i) => i >= count);
+    const arrLen = array.length;
+
+    if (count === 0 ||
+        count > arrLen ||
+        count < -arrLen) return;
+
+    const ret = array.slice(Math.abs(count), arrLen);
+    return ret;
 };
 
 const unshift = (array, ...values) => {
@@ -34,8 +45,10 @@ assert.deepStrictEqual(pop(arr), 4);
 assert.deepStrictEqual(pop(arr, 2), [3, 4]);
 assert.deepStrictEqual(pop(arr, 3), [2, 3, 4]);
 assert.deepStrictEqual(pop(arr, arr.length), [1, 2, 3, 4]);
+assert.deepStrictEqual(pop(arr, -arr.length + 1), [2, 3, 4]);
+assert.deepStrictEqual(pop(arr, -1), 4);
+assert.deepStrictEqual(pop(arr, -arr.length), undefined);
 assert.deepStrictEqual(pop(arr, 0), undefined); // wrong input
-assert.deepStrictEqual(pop(arr, -1), undefined); // wrong input
 assert.deepStrictEqual(pop(arr, arr.length + 1), undefined); // wrong input
 assert.deepStrictEqual(arr, [1, 2, 3, 4]);
 assert.deepStrictEqual(unshift(arr, 0), [0, 1, 2, 3, 4]);
@@ -44,8 +57,9 @@ assert.deepStrictEqual(unshift(arr), [1, 2, 3, 4]); // wrong input
 assert.deepStrictEqual(arr, [1, 2, 3, 4]);
 assert.deepStrictEqual(shift(arr), [2, 3, 4]);
 assert.deepStrictEqual(shift(arr, 2), [3, 4]);
+assert.deepStrictEqual(shift(arr, -1), [2, 3, 4]);
 assert.deepStrictEqual(shift(arr, arr.length), []);
+assert.deepStrictEqual(shift(arr, -arr.length), []);
 assert.deepStrictEqual(shift(arr, 0), undefined); // wrong input
-assert.deepStrictEqual(shift(arr, -1), undefined); // wrong input
 assert.deepStrictEqual(shift(arr, arr.length + 1), undefined); // wrong input
 assert.deepStrictEqual(arr, [1, 2, 3, 4]);
