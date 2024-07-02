f3910b7706c03cb1e62097a6d3ce8ec79114e4a2
seungBeomHan <essence.of.study@gmail.com>
2024-07-02 15:17:14 +0900

    [main / SUCCESS] 구현 및 테스트 완료

--- src/homework/06.29_7.js
+++ src/homework/06.29_7.js
@@ -6,17 +6,46 @@ const C = [11, 222, 3, 4, 555];
 
 // 교집합
 const intersection = (setA, setB) => {
-    return [...new Set(...setA, ...setB)];
+    const cache = new Map();
+    const result = new Set();
+
+    setB.forEach(e => {
+        cache.set(e, e);
+    });
+
+    setA.forEach((e) => {
+        const value = cache.get(e);
+        if (value !== undefined) result.add(e);
+    });
+
+    return [...result];
 };
 
 // 차집합
 const diff = (setA, setB) => {
+    const cache = new Map();
+    const result = new Set();
 
+    setB.forEach(e => {
+        cache.set(e, e);
+    });
+
+    setA.forEach((e) => {
+        const value = cache.get(e);
+        if (value === undefined) result.add(e);
+    });
+
+    return [...result];
 };
 
 // 합집합 
-const union = (setA, setB) => {
-
-};
+const union = (setA, setB) => [...setB.reduce((set, cur) => set.add(cur), new Set(setA))];
 
 assert.deepStrictEqual(intersection(A, B), [1, 3, 5]);
+assert.deepStrictEqual(intersection(A, C), [3, 4]);
+assert.deepStrictEqual(diff(A, B), [2, 4]);
+assert.deepStrictEqual(diff(B, A), [22, 44]);
+assert.deepStrictEqual(diff(A, C), [1, 2, 5]);
+assert.deepStrictEqual(diff(B, C), [1, 22, 44, 5]);
+assert.deepStrictEqual(union(A, B), [1, 2, 3, 4, 5, 22, 44]);
+assert.deepStrictEqual(union(A, C), [1, 2, 3, 4, 5, 11, 222, 555]);
