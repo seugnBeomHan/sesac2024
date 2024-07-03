156cf8409ca7c58eb8e39ba641987d5133afac78
seungBeomHan <essence.of.study@gmail.com>
2024-07-04 01:21:00 +0900

    [main / SUCCESS] 구현 및 테스트 완료

--- src/homework/0702_4.js
+++ src/homework/0702_4.js
@@ -6,22 +6,22 @@ const upperToLower = (str) => str.replace(/[A-Z]/g, `*$&*-`).toLowerCase();
 console.log(upperToLower('Senior Coding Learning JS'));
 
 // 2
-console.log('Senior Coding Learning JS'.replace(/[A-Z]/g, '#')); // '#enior #oding #earning ##'
-console.log('Senior Coding Learning JS'.replace(/([A-Z]+)([a-z\s]*)/g, '$1-')); // 'S-C-L-JS-'
-console.log('Senior Coding Learning JS'.replace(/([A-Z]+)([a-z\s]*)/g, '$1-$2')); // 'S-enior C-oding..
-console.log('Senior Coding Learning JS'.replace(/[A-Z]/g, '`$&`'));    // '`S`enior `C`oding `L`earning `J``S`'
-console.log('1234-2323-2323-2323'.replace(/(\d{4})-(\d{4})-(.*)$/, '$1-####-$3'));
+const telfmt = (tel) => {
+    const getTelNumber = (start, end) => `${tel.slice(start, end)}-${tel.slice(end)}`;
 
+    const TEL_LENGTH = tel.length;
+    const LOCAL_NUMBER = tel[1];
 
-const telfmt = (telNum) => {
-
+    tel = getTelNumber(0, TEL_LENGTH - 4);
+    if (TEL_LENGTH % 4 === 0) return tel[4] !== '-' ? getTelNumber(0, 4) : tel;
+    return LOCAL_NUMBER === '2' ? getTelNumber(0, 2) : getTelNumber(0, 3);
 };
 
-assert.deepStrictEqual(telfmt('15771577'), '1577-1577');          //         (8)
-assert.deepStrictEqual(telfmt('050712345678'), '0507-1234-5678'); // 8 - 4 4(12)
-assert.deepStrictEqual(telfmt('01012345678'), '010-1234-5678');   // 7 - 3 4(11)
-assert.deepStrictEqual(telfmt('07012341234'), '070-1234-1234');   // 7 - 3 4(11)
+assert.deepStrictEqual(telfmt('021234567'), '02-123-4567');       // 5 - 2 3(9)
 assert.deepStrictEqual(telfmt('0101234567'), '010-123-4567');     // 6 - 3 3(10)
 assert.deepStrictEqual(telfmt('0331234567'), '033-123-4567');     // 6 - 3 3(10)
 assert.deepStrictEqual(telfmt('0212345678'), '02-1234-5678');     // 6 - 2 4(10)
-assert.deepStrictEqual(telfmt('021234567'), '02-123-4567');       // 5 - 2 3(9)
+assert.deepStrictEqual(telfmt('01012345678'), '010-1234-5678');   // 7 - 3 4(11)
+assert.deepStrictEqual(telfmt('07012341234'), '070-1234-1234');   // 7 - 3 4(11)
+assert.deepStrictEqual(telfmt('050712345678'), '0507-1234-5678'); // 8 - 4 4(12)
+assert.deepStrictEqual(telfmt('15771577'), '1577-1577');          //         (8)
