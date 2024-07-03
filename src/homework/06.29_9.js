f2101dd9bed6ecf8178051b059ec64b9b43d904c
seungBeomHan <essence.of.study@gmail.com>
2024-07-03 09:31:00 +0900

    [main / REFACT] 변수 간소화 및 이너 함수 재배치

--- src/homework/06.29_9.js
+++ src/homework/06.29_9.js
@@ -1,39 +1,6 @@
 const printCalender = (date) => {
     if (typeof date !== 'string') return;
 
-    const makeCalender = () => {
-        const calender = [['일', '월', '화', '수', '목', '금', '토']];
-        let oneWeek = [];
-
-        for (let i = 1; i <= MONTH_OF_FULL_DAYS[month - 1]; i += 1) {
-            oneWeek[startDayOfMonth] = i;
-
-            if ((startDayOfMonth += 1) === WEEK_COUNT) {
-                calender.push(oneWeek);
-                startDayOfMonth = 0;
-                oneWeek = [];
-            }
-        }
-        calender.push(oneWeek);
-        return calender;
-    };
-
-    const draw = () => {
-        console.log(`\n${inputDate.getFullYear()}년 ${month}월`)
-        console.table(makeCalender());
-    };
-
-    const getInputDate = () => {
-        return new Date(new Date().getFullYear(), month - 1, day);
-    }
-
-    const getStartDayOfMonth = () => {
-        const result = inputDate.getDay() - ((day - 1) % WEEK_COUNT);
-        return result < 0 ?
-            result + WEEK_COUNT :
-            result;
-    };
-
     const WEEK_COUNT = 7;
     const MONTH_OF_FULL_DAYS = {
         0: 31,
@@ -50,27 +17,60 @@ const printCalender = (date) => {
         11: 31,
     };
 
-    const [month, day] = date.split('/');
-    const inputDate = getInputDate();
-    let startDayOfMonth = getStartDayOfMonth();
+    const [month, day] = date.split(new RegExp('/|\\.|-'));
+
+    const makeCalender = () => {
+        const CUR_MONTH = MONTH_OF_FULL_DAYS[month - 1];
+        const calender = [['일', '월', '화', '수', '목', '금', '토']];
+        let oneWeek = [];
+        let startDayOfMonth = getStartDayOfMonth();
+
+        for (let i = 1; i <= CUR_MONTH; i += 1) {
+            oneWeek[startDayOfMonth] = i;
+
+            if ((startDayOfMonth += 1) === WEEK_COUNT) {
+                calender.push(oneWeek);
+                startDayOfMonth = 0;
+                oneWeek = [];
+            }
+        }
+        calender.push(oneWeek);
+        return calender;
+    };
+
+    const draw = () => {
+        console.log(`\n${month}월`);
+        console.table(makeCalender());
+    };
+
+    const getStartDayOfMonth = () => {
+        const startDayOfMonth = makeDateObj().getDay() - ((day - 1) % WEEK_COUNT);
+        return startDayOfMonth < 0 ?
+            startDayOfMonth + WEEK_COUNT :
+            startDayOfMonth;
+    };
+
+    const makeDateObj = () => {
+        return new Date(new Date().getFullYear(), month - 1, day);
+    }
 
     draw();
 };
 
 /**
- * 입력 양식 제한 (년도는 올해)
- * string: mm/dd
+ * 입력 양식 (년도는 올해)
+ * string: mm/dd | mm.dd | mm-dd
  */
 const today = new Date();
-printCalender(`${today.getMonth() - 5}/${14}`);
-printCalender(`${today.getMonth() - 4}/${22}`);
-printCalender(`${today.getMonth() - 3}/${30}`);
+printCalender(`${today.getMonth() - 5}-${14}`);
+printCalender(`${today.getMonth() - 4}-${22}`);
+printCalender(`${today.getMonth() - 3}-${30}`);
 printCalender(`${today.getMonth() - 2}/${5}`);
 printCalender(`${today.getMonth() - 1}/${1}`);
 printCalender(`${today.getMonth()}/${15}`);
-printCalender(`${today.getMonth() + 1}/${today.getDay()}`);
-printCalender(`${today.getMonth() + 2}/${25}`);
-printCalender(`${today.getMonth() + 3}/${18}`);
-printCalender(`${today.getMonth() + 4}/${6}`);
-printCalender(`${today.getMonth() + 5}/${3}`);
-printCalender(`${today.getMonth() + 6}/${15}`);
+printCalender(`${today.getMonth() + 1}.${today.getDay()}`);
+printCalender(`${today.getMonth() + 2}.${25}`);
+printCalender(`${today.getMonth() + 3}.${18}`);
+printCalender(`${today.getMonth() + 4}-${6}`);
+printCalender(`${today.getMonth() + 5}-${3}`);
+printCalender(`${today.getMonth() + 6}-${15}`);
