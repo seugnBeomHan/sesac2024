a1f3a573a9d23b6a40ad3635341ad55588f9481b
seungBeomHan <essence.of.study@gmail.com>
2024-07-04 09:30:51 +0900

    [main / SUCCESS] 구현 및 테스트 완료

--- src/homework/0702_3.js
+++ src/homework/0702_3.js
@@ -1,19 +1,24 @@
 import assert from 'assert/strict';
 
-const searchByKoreanInitialSound = (datas, initConsonant) => {
+const searchByKoreanInitialSound = (datas, initConsonants) => {
     const makeJamoRange = () => {
         const result = [];
 
-        for (const word of initConsonant) {
-            const wordIndex = INITIAL_CONSONANT.indexOf(word);
+        for (const consonant of initConsonants) {
+            const asciiCode = consonant.charCodeAt(0);
+            if (asciiCode <= 127) {
+                result.push([asciiCode, asciiCode]);
+                continue;
+            }
+            const wordIndex = INITIAL_CONSONANT.indexOf(consonant);
             const jamoStart = GA + (INITIAL_INTERVAL * wordIndex);
-            const jamoEnd = jamoStart + SAME_INITIAL_INTERVAL;
+            const jamoEnd = jamoStart + INITIAL_INTERVAL - 1;
             result.push([jamoStart, jamoEnd]);
         }
-
         return result;
     };
 
+    // 초성 모음
     const INITIAL_CONSONANT = [
         'ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ',
         'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ',
@@ -22,30 +27,45 @@ const searchByKoreanInitialSound = (datas, initConsonant) => {
     ];
     const GA = 44032; // 가
     const INITIAL_INTERVAL = 588; // 가 ~ 나 간격
-    const SAME_INITIAL_INTERVAL = 587; // 가 ~ 깋 간격
     const jamoRange = makeJamoRange();
 
     return datas.reduce((result, word) => {
         for (let i = 0; i < word.length; i += 1) {
             let unicode = word[i].charCodeAt(0);
+            let isValid = true;
 
-            if (unicode >= jamoRange[0][0] && unicode <= jamoRange[0][1]) {
-                unicode = word[i += 1]?.charCodeAt(0);
-
-                if (unicode >= jamoRange[1][0] && unicode <= jamoRange[1][1]) {
-                    result.push(word);
-                    return result;
+            for (const [start, end] of jamoRange) {
+                if (unicode < start || unicode > end || unicode === undefined) {
+                    isValid = false;
+                    break;
                 }
+                unicode = word[i += 1]?.charCodeAt(0);
+            }
+
+            if (isValid) {
+                result.push(word);
+                return result;
             }
         }
         return result;
     }, []);
 };
 
-const datas = ['강원도 고성군', '고성군 토성면', '토성면 북면', '북면', '김1수'];
-assert.deepStrictEqual(searchByKoreanInitialSound(datas, 'ㄱㅇ'), ['강원도 고성군']);
+const datas = ['강원도 고성군', '고성군 토성면', '토성면 북면', '북면', '김1수'
+    , '네이버', '다음', '아스키코드표', '아스키 코드표', '유니코드', '한글 초성', 'ASCII 코드표'];
+assert.deepStrictEqual(searchByKoreanInitialSound(datas, 'ㄴㅇ'), ['네이버']);
+assert.deepStrictEqual(searchByKoreanInitialSound(datas, 'ㄷㅇ'), ['다음']);
+assert.deepStrictEqual(searchByKoreanInitialSound(datas, 'ㄱ ㅊ'), ['한글 초성']);
+assert.deepStrictEqual(searchByKoreanInitialSound(datas, 'ㅇㄴ'), ['유니코드']);
+assert.deepStrictEqual(searchByKoreanInitialSound(datas, 'ㅇㅅㅋ'), ['아스키코드표', '아스키 코드표']);
+assert.deepStrictEqual(searchByKoreanInitialSound(datas, 'ㅇㅅㅋ '), ['아스키 코드표']);
+assert.deepStrictEqual(searchByKoreanInitialSound(datas, 'ASC'), ['ASCII 코드표']);
+assert.deepStrictEqual(searchByKoreanInitialSound(datas, 'ㄷ ㄱ'), ['강원도 고성군']);
+assert.deepStrictEqual(searchByKoreanInitialSound(datas, 'ㄱ ㅌ'), ['고성군 토성면']);
 assert.deepStrictEqual(searchByKoreanInitialSound(datas, 'ㄱㅅㄱ'), ['강원도 고성군', '고성군 토성면']);
 assert.deepStrictEqual(searchByKoreanInitialSound(datas, 'ㅌㅅㅁ'), ['고성군 토성면', '토성면 북면']);
 assert.deepStrictEqual(searchByKoreanInitialSound(datas, 'ㅂㅁ'), ['토성면 북면', '북면']);
 assert.deepStrictEqual(searchByKoreanInitialSound(datas, 'ㅍㅁ'), []);
+assert.deepStrictEqual(searchByKoreanInitialSound(datas, 'ㄱㄴ'), []);
+assert.deepStrictEqual(searchByKoreanInitialSound(datas, 'ㅅㅁ'), ['고성군 토성면', '토성면 북면']);
 assert.deepStrictEqual(searchByKoreanInitialSound(datas, 'ㄱ1ㅅ'), ['김1수']);
