7b62f8e12c6a1d992b4441ce7077dc491c913da5
seungBeomHan <essence.of.study@gmail.com>
2024-07-12 20:53:05 +0900

    [main / FINISH] 2024.07.12 ts 학습

--- src/TS/src/ex/ex1.ts
+++ src/TS/src/ex/ex1.ts
@@ -10,3 +10,25 @@ function add(num1: number, num2: number, num3?: number) {
 
 console.log(add(1, 2));
 console.log(add(1, 2, 3));
+
+const isNumberInputAny = (input: any) => typeof input === 'number';
+let numAndString = Math.random() > 0.5 ? 10 : 'han';
+
+if (isNumberInputAny(numAndString)) {
+    console.log(numAndString.constructor.name);
+}
+
+const tuple = [10, 20, '30'] as const;
+const namedTuple: [name: string, age: number] = ['han', 27];
+
+const han = { name: 'han', age: 27 };
+let seung: keyof typeof han = Math.random() < 0.5 ? 'name' : 'age';
+console.log(han[seung]);
+
+class aaa {
+    readonly id;
+    constructor(id: number) {
+        this.id = id;
+        this.id = 10;
+    }
+}
