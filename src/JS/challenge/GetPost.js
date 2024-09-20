81298422f047b534443d83b694ff6fc6c779f710
seungBeomHan <essence.of.study@gmail.com>
2024-09-20 16:16:22 +0900

    [UPDATE] 폴더 명 수정

--- src/JS/challenge/GetPost.js
+++ src/JS/challenge/GetPost.js
@@ -1,20 +1,29 @@
-const postURL = 'https://jsonplaceholder.typicode.com/posts?userId=1';
-const comentURL = `https://jsonplaceholder.typicode.com/posts/<postId>/comments`;
+const postURL = "https://jsonplaceholder.typicode.com/posts?userId=1";
 
 const getPosts = async () => {
-    const httpPosts = await fetch(postURL);
-    const posts = await httpPosts.json();
-    const httpComents = await Promise.all(posts.map((post) => {
-        return fetch(`https://jsonplaceholder.typicode.com/posts/${post.id}/comments`);
-    }));
-    const coments = await Promise.all(httpComents.map((coment) => {
-        return coment.json();
-    }));
-    return [posts, coments];
-}
+  const httpPosts = await fetch(postURL);
+  const posts = await httpPosts.json();
+  const httpComents = await Promise.all(
+    posts.map((post) => {
+      return fetch(
+        `https://jsonplaceholder.typicode.com/posts/${post.id}/comments`
+      );
+    })
+  );
+  const coments = await Promise.all(
+    httpComents.map((coment) => {
+      return coment.json();
+    })
+  );
+  return [posts, coments];
+};
 
-getPosts().then((arr) => {
+getPosts()
+  .then((arr) => {
     const [posts, coments] = arr;
     console.log(posts);
     console.log(coments);
-}).catch((err) => { console.log(err); });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
